a138c0bfe0ee310fcf42ea1de210606d748c54a8
gokulrajanoff <gokulrajanoff@gmail.com>
2018-06-25 20:20:49 +0530

    updated

--- DynamicTableComponentController.js
+++ DynamicTableComponentController.js
@@ -1,7 +1,13 @@
 ({
-	getRecordsForTable : function(component, event, helper) {
+    getRecordsForTable : function(component, event, helper) {
         var action=component.get("c.Get_Record_For_Table_Input_From_Query");
-        action.setParams({"objectName":"contact","queryFields":"name,title,phone,email","fullQuery":"select name,title,phone,email from contact"});
+        var Object=component.get("v.Object");
+        var queryFields=component.get("v.queryFields");
+        var fullQuery=component.get("v.fullQuery");
+        console.log("Object"+Object+"queryFields"+queryFields+"fullQuery"+fullQuery);
+        //  action.setParams({"objectName":"contact","queryFields":"name,title,phone,email","fullQuery":"select name,title,phone,email from contact"});
+        action.setParams({"objectName":Object,"queryFields":queryFields,"fullQuery":fullQuery});
+        
         action.setCallback(this,function(response)
                            {
                                component.set("v.Table_header_Records",response.getReturnValue());
@@ -15,13 +21,81 @@
                                if (records!=null && records.ListOfEachRecord.length == 0) {
                                    component.set("v.Message", true);
                                    component.set("v.showHeaders",false);
-
+                                   
                                } else {
                                    component.set("v.Message", false);
                                    component.set("v.showHeaders",true);
                                }
                            });
         $A.enqueueAction(action);
+    },
+    keyup:function(component, event, helper) {
+        var searchKeyFld = component.find("searchId");
+        var srcValue = searchKeyFld.get("v.value");
+        console.log("srcvalue"+srcValue);
+        console.log("keyupfunction"+event.keyCode); 
+        console.log(component.get("v.Table_header_Records"));
+        var tableRecords=component.get("v.Table_header_Records");       
+        if ((srcValue == '' || srcValue == null) && (event.keyCode === 13)) {
+            alert("Enter some value to Search");
+        }
+        else{
+            
+            if(tableRecords!=null)
+            {
+                if(tableRecords.SwapVariable==null)
+                {
+                    tableRecords.SwapVariable=tableRecords.ListOfEachRecord;
+                }
+                else
+                {
+                    tableRecords.ListOfEachRecord=tableRecords.SwapVariable;
+                }
+                var headerRecords = tableRecords.headerList;
+                var Index=0;
+                var Sorted=1;
+                for(var i=0;i<headerRecords.length;i++){
+                    if(headerRecords[i].sorted!=2)
+                    {
+                        Index=headerRecords[i].index;
+                        Sorted=headerRecords[i].sorted;
+                    }
+                }
+                var originalRecords=tableRecords.ListOfEachRecord;
+                var searchRelatedRecords=[];
+                for(var i=0;i<tableRecords.ListOfEachRecord.length;i++){
+                    var matchFound=false;
+                    for(var j=0;j<tableRecords.ListOfEachRecord[i].recordValue.length;j++)
+                    {
+                        var value = tableRecords.ListOfEachRecord[i].recordValue[j];
+                        if(value.toString().toLowerCase().indexOf(srcValue)!= -1)
+                        {
+                            matchFound=true;
+                        }
+                    }
+                    if(matchFound)
+                    {
+                        searchRelatedRecords.push(tableRecords.ListOfEachRecord[i]);
+                        if(Sorted==1){
+                            searchRelatedRecords.sort(function(a,b) {return (a['recordValue'][Index].toString().toLowerCase()  > b['recordValue'][Index].toString().toLowerCase() ) ? 1 : ((b['recordValue'][Index].toString().toLowerCase()  > a['recordValue'][Index].toString().toLowerCase() ) ? -1 : 0);} );
+                            
+                        }
+                        else
+                        {
+                            searchRelatedRecords.sort(function(a,b) {
+                                return (a['recordValue'][Index].toString().toLowerCase()  < b['recordValue'][Index].toString().toLowerCase() ) ? 1 : ((b['recordValue'][Index].toString().toLowerCase()  < a['recordValue'][Index].toString().toLowerCase() ) ? -1 : 0);} );
+                            
+                            
+                        }
+                    }
+                }
+                tableRecords.SwapVariable=originalRecords;
+                tableRecords.ListOfEachRecord=[];
+                tableRecords.ListOfEachRecord=searchRelatedRecords;
+                component.set("v.Table_header_Records",tableRecords); 
+            }
+        }
+        
     },
     sortColumn : function(component, event, helper) {
         var ctarget = event.currentTarget;
@@ -47,11 +121,11 @@
              else if(sorted==0 || sorted==2)
              {
                  console.log("ascending");
-                 objectRecord.sort(function(a,b) {return (a['recordValue'][headerRecord[i].index].toString().toLowerCase()  > b['recordValue'][headerRecord[i].index].toString().toLowerCase() ) ? 1 : ((b['recordValue'][headerRecord[i].index].toString().toLowerCase()  > a['recordValue'][headerRecord[i].index].toString().toLowerCase() ) ? -1 : 0);} );
+                 objectRecord.sort(function(a,b) {return (a['recordValue'][headerRecord[i].index].toString().toLowerCase()  > b['recordValue'][headerRecord[i].index].toString().toLowerCase() ) ? 1 : ((b['recordValue'][headerRecord[i].index].toString().toLowerCase()  > a['recordValue'][headerRecord[i].index].toString().toLowerCase() ) ? -1 : 0);} ); 
                  sorted=1;
              }
-             headerRecord[i].sorted=sorted;
-
+             headerRecord[i].sorted=sorted; 
+             
             }
             else{
                 headerRecord[i].sorted=2;
@@ -61,11 +135,14 @@
         Wrapedrecords.headerList=headerRecord;
         console.log(Wrapedrecords);
         component.set("v.Table_header_Records",Wrapedrecords);
-
-
+        
+        
     },
     DOSomeThingOnCLickOfARow : function(component, event, helper) {
-
+        var ctarget = event.currentTarget;
+        //we can get the record id of the row in table.
+        var id_str = ctarget.dataset.value;
+        
     }
-
+    
 })
